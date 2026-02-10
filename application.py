53daa9689678913d3b8f066df64c544722113f98
Divyanshu Kumar <dkumar0847607@gmail.com>
2026-02-10 20:28:46 +0530

    Fix EB deployment issues

--- application.py
+++ application.py
@@ -47,7 +47,8 @@ def predict_datapoint():
   
     
 if __name__ == "__main__":
-    application.run(host="127.0.0.1", port=5050, debug=False)
+    application.run(host="0.0.0.0", port=8080)
+
         
 
 
